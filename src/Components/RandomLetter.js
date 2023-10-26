79b8d17cc7fb6f83d6ae6830182f7eca36c5eaad
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2023-10-26 15:02:51 -0500

    add random letter

---
+++ src/Components/RandomLetter.js
@@ -0,0 +1,13 @@
+import alphabet from "../Alphabet";
+let letter
+let Letters = document.getElementsByClassName('Letters')
+
+let randomLetter =()=>{
+    letter= alphabet[Math.floor(Math.random() * alphabet.length)]
+    console.log(letter)
+
+    return Letters.innerHTML= letter
+
+}
+randomLetter()
+export default randomLetter
