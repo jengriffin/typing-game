e7bccd4891d99935a35c2447241460563b2bdddf
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2024-02-26 13:58:26 -0600

    Now class name is in place and displaying letter properly

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,18 +3,20 @@ import letter from "./RandomLetter"
 import randomLetter from "./RandomLetter";
 import RandomLetter from "./RandomLetter";
 
-let Game = (props) => {
-    // let Letters = document.getElementsByClassName('Letters');
-    // const generateRandomLetter = () => {
-    //     return alphabet[Math.floor(Math.random() * alphabet.length)]
-    // }
-    //
-    // let letter = generateRandomLetter();
+
+let Game = () => {
+    let Letters = document.getElementsByClassName('Letters');
+    const generateRandomLetter = () => {
+        return alphabet[Math.floor(Math.random() * alphabet.length)]
+    }
+
+    let letter = generateRandomLetter();
 
     document.addEventListener('keypress', (event) => {
         let key = String.fromCharCode(event.keyCode)
         console.log(key)
-        console.log({letter:props})
+        console.log(letter)
+
          if (letter === key) {
              console.log("Hello! Colmack sucks")
 
@@ -27,7 +29,7 @@ let Game = (props) => {
      * then the Game component renders the Letter component (I guess)
      * so Letter is a child of Game
      */
-    // return (<p>{letter}</p>)
-    return <RandomLetter/>
+    // return
+    return <div className="bubble"><p>{letter}</p></div>
 }
 export default Game
