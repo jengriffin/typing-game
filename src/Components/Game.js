9a008eeba82729b313c0bba80913e52c878287c7
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2024-08-26 16:31:10 -0500

    working with new id system!

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -7,36 +7,44 @@ import RandomLetter from "./RandomLetter";
 
 let Game = () => {
     let Letters = document.getElementsByClassName('Letters');
+
     const generateRandomLetter = () => {
         return alphabet[Math.floor(Math.random() * alphabet.length)]
     }
-    const [input, setInput] = useState('');
+    // const [input, setInput] = useState('');
 
     let letter = generateRandomLetter();
+    <div className="bubble" id="type">letter</div>
+    console.log("hello")
+
 
     document.addEventListener('keypress', (event) => {
         let key = String.fromCharCode(event.keyCode)
-        console.log(key)
-        console.log(letter)
+        console.log("Key press: " + key)
+        console.log("Desired letter: " + letter)
 
+        // for (;;) {
+            if (letter === key) {
+                letter = generateRandomLetter()
 
-         if (letter === key) {
-             let letter = '';
-             generateRandomLetter();
-             location.reload(true)
-                 console.log("Hello! Colmack sucks")
+                 //letter=document.getElementsByClassName("bubble");
+                document.getElementById("type").textContent=letter;
+                console.log("Hello! Colmack sucks")
+             } else {
+                console.log("Yas")
+                console.log("Yas letter: " + letter)
+            }
 
-         } else {
-             console.log(letter)
-         }
     })
     /* try making a Letter component
      * Letter component should accept a random letter and display it
      * then the Game component renders the Letter component (I guess)
      * so Letter is a child of Game
+     * Grab div by classname to update in the correct loop. Jury out about whether or not
+     * a loop is needed.
      */
     // return
-    return <div className="bubble">{letter}</div>
+    return <div className="bubble" id="type">{letter}</div>
 
 }
 export default Game
